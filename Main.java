9bd5034d70f2f541dde4c29b96859ae80e452fbd
Van1ak <43353756+Van1ak@users.noreply.github.com>
2018-12-09 12:10:10 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,41 +6,21 @@ public class Main {
 
      public static void main(String[] args) {
           Scanner scanner = new Scanner(System.in);
-          Point Point = new Point();
-          System.out.println("enter point's X coordinate");
-          Point.x = scanner.nextInt();
-          System.out.println("enter point's Y coordinate");
-          Point.y = scanner.nextInt();
+          System.out.println("Enter point's coordinates ");
+          Point Point = new Point(scanner.nextInt(), scanner.nextInt());
           Point.coordinates();
           Point.tobegin();
-          System.out.println("Enter skolko zhelaete add k X");
-          Point.a = scanner.nextInt();
-          System.out.println("Enter skolko zhelaete add k Y");
-          Point.b = scanner.nextInt();
-          Point.add();
 
-          System.out.println("Enter line's first point's coordinates ");
-          Line.x1 = scanner.nextInt();
-          Line.y1 = scanner.nextInt();
-          System.out.println("Enter line's second point's coordinates ");
-          Line.x2 = scanner.nextInt();
-          Line.y2 = scanner.nextInt();
-          Line line = new Line();
+          System.out.println("Enter line's coordinates ");;
+          Line line = new Line(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
           Line.coordinates();
           Line.length();
           Line.checkpoint();
           Line.oxangle();
 
-          System.out.println("Enter broken's first point's coordinates ");
-          BrokenLine.x1 = scanner.nextInt();
-          BrokenLine.y1 = scanner.nextInt();
-          System.out.println("Enter broken's second point's coordinates ");
-          BrokenLine.x2 = scanner.nextInt();
-          BrokenLine.y2 = scanner.nextInt();
-          System.out.println("Enter broken's third point's coordinates ");
-          BrokenLine.x3 = scanner.nextInt();
-          BrokenLine.y3 = scanner.nextInt();
-          BrokenLine brokenLine = new BrokenLine();
+          System.out.println("Enter broken's coordinates ");
+          BrokenLine brokenLine = new BrokenLine(scanner.nextInt(), scanner.nextInt(),
+                  scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
           BrokenLine.coordinates();
           BrokenLine.angle();
           BrokenLine.check();
