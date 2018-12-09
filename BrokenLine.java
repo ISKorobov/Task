8b0d7dd0a2eda7dc98ef7438f336122558e37f61
Van1ak <43353756+Van1ak@users.noreply.github.com>
2018-12-09 12:13:07 +0300

    Update BrokenLine.java

--- BrokenLine.java
+++ BrokenLine.java
@@ -1,25 +1,24 @@
 package com.company;
 
 public class BrokenLine extends Line {
-    BrokenLine broken = new BrokenLine();
     public int x3, y3;
+    BrokenLine broken = new BrokenLine(x1, y1, x2, y2, x3, y3);
 
-    public BrokenLine (int x1, int y1, int x2, int y2) {
-        super.x1 = line.x1;
-        super.y1 = line.y1;
-        super.x2 = line.x2;
-        super.y2 = line.y2;
-        System.out.println("Third point's coordinates");
-        x3 = scanner.nextInt();
-        y3 = scanner.nextInt();
+    public BrokenLine (int x1, int y1, int x2, int y2, int x3, int y3) {
+        super.x1 = x1;
+        super.y1 = y1;
+        super.x2 = x2;
+        super.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
     }
-    @Override
+
     public BrokenLine coordinates() {
 
         return broken;
     }
 
-    public void angle() {
+    public double angle() {
         double a = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
         double b = Math.sqrt((x2-x3)*(x2-x3)+(y2-y3)*(y2-y3));
         double c = Math.sqrt((x3-x1)*(x3-x1)+(y3-y1)*(y3-y1));
@@ -27,10 +26,10 @@ public class BrokenLine extends Line {
         Angle = (a*a + b*b - c*c) / 2 * a * c;
         Angle = Math.acos(Angle);
         Angle = Angle * 90 / Math.PI;
-        System.out.println("Angle line otnositel'no OX is " + Angle);
+        return Angle;
     }
 
-    public boolean check() {
+    public  boolean check() {
         if( (Line.checkpoint(x1, y1, x2, y2)) == false) {
             Line.checkpoint(x2, y2, x3, y3);
         } else {
